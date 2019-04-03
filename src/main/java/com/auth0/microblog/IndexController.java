506882cc1df6f3ba35dec9e81a2f5fa9859d2f99
Sistemas <sistemas@consorcioambiental.com>
2019-04-03 16:58:45 -0700

    first commit

---
+++ src/main/java/com/auth0/microblog/IndexController.java
@@ -0,0 +1,20 @@
+package com.auth0.microblog;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+@RequestScoped
+public class IndexController {
+
+    @Inject
+    private MicroPostsService microPostsService;
+
+    public List<MicroPost> getMicroPosts() {
+        return microPostsService.getMicroPosts();
+    }
+
+}
